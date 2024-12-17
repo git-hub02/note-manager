fcf87f52f95305c1e3e0a455d9721e8d94f8f9b0
git-hub02 <cickapopa1@gmail.com>
2024-12-18 01:15:35 +0700

    Add files via upload

---
+++ date_changer.py
@@ -0,0 +1,14 @@
+# Имена
+name = input('напишите имя пользователя: ')
+title = input('напишите заголовок: ')
+content = input('напишшите описание: ')
+status = input('напишите статус: ')
+created_date = input('напишите дату создания в формате день-месяц-год: ')
+issue_date = input('напишите дату истечения в форматедень-месяц-год: ')
+# вывод значений
+print ("Имя:", name)
+print ("Заголовок:",title)
+print ("Описание:",content)
+print ("Статус:",status)
+print ("Дата создания:",created_date)
+print ("Дата истечения:",issue_date)
---
+++ greeting.py
@@ -0,0 +1,14 @@
+# Имена
+name='ruslan'
+title='training'
+content='complete the task'
+status='active'
+created_date='17-11-2024' #день-месяц-год
+issue_date='20-11-2024' #день-месяц-год
+# вывод значений
+print("Имя:", name)
+print("Заголовок:",title)
+print("Описание:",content)
+print("Статус:",status)
+print("Дата создания:",created_date)
+print("Дата истечения:",issue_date)
